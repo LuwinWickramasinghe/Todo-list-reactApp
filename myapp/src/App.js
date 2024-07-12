d2098d1e859c163a3e3366d0d734a4bc7b8fea83
iamluwin <luwin.anuka@gmail.com>
2024-07-12 23:45:00 +0530

    added css

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,21 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className = "card-header">
+          Todo List
+        </div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">no</th>
+                <th scope="col">description</th>
+                <th scope="col">assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Feed coco</td>
+                <td>assigned</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>get haircut</td>
+                <td>luwin</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
